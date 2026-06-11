6563024954809acb3e4d4dbca7730cc8e2f767d2
nandhinis14102007-rgb <nandhinis14102007@gmail.com>
2026-06-11 19:03:12 +0530

    functions.c

--- functions.c
+++ functions.c
@@ -1,1 +1,46 @@
-//function program will be added later//
+#include <stdio.h> 
+void frequencycount(int arr[] ,int n);
+int main()
+{
+     int n;
+     scanf("%d",&n);
+     int arr[n];
+     for(int i=0;i<n;i++) 
+     {
+         scanf("%d",&arr[i]);
+     }
+     isduplicate(arr,n);
+     return 0;
+}
+void frequencycount(int arr[],int n)
+{  
+    int i=0;
+    int duplicate=0; 
+    int count=0; 
+     for(int i=0;i<n;i++) 
+     { 
+         duplicate=0; 
+        for(int j=0;j<i;j++)
+        {
+            if(arr[i]==arr[j]) 
+            {
+             duplicate=1;
+             break;
+            }
+         }
+    if(duplicate==1)
+    {
+        continue;
+    }
+         count=0;
+    for(int k=0;k<n;k++) 
+    {
+     if(arr[i]==arr[k])
+      {
+       count++; 
+       } 
+    }
+     printf("%d-> %d\n",arr[i],count);
+}
+    
+    }
